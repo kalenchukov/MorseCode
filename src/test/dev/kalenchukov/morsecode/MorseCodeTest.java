6fc02acbbff29fe752dd45dc22d3a1c76bf0fd72
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2023-02-06 00:27:51 +0300

    Изменена версия требуемого модуля junit до 5.8.2

--- src/test/dev/kalenchukov/morsecode/MorseCodeTest.java
+++ src/test/dev/kalenchukov/morsecode/MorseCodeTest.java
@@ -26,9 +26,9 @@ package dev.kalenchukov.morsecode;
 
 import dev.kalenchukov.morsecode.schemes.EnglishScheme;
 import dev.kalenchukov.morsecode.schemes.RussianScheme;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MorseCodeTest
 {
