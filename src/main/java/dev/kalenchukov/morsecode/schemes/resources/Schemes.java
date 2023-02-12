613c6ed2a2315427d9e32ae534f9ac86fcd911b0
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2023-02-12 12:43:05 +0300

    Добавлено перечисление схем сопоставления Schemes

---
+++ src/main/java/dev/kalenchukov/morsecode/schemes/resources/Schemes.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright © 2023 Алексей Каленчуков
+ * GitHub: https://github.com/kalenchukov
+ * E-mail: mailto:aleksey.kalenchukov@yandex.ru
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.kalenchukov.morsecode.schemes.resources;
+
+import dev.kalenchukov.morsecode.schemes.EnglishScheme;
+import dev.kalenchukov.morsecode.schemes.RussianScheme;
+import dev.kalenchukov.morsecode.schemes.Schematic;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Перечисление схем сопоставления букв.
+ */
+public enum Schemes
+{
+	/**
+	 * Буквы русского алфавита.
+	 */
+	RUSSIAN(new RussianScheme()),
+
+	/**
+	 * Буквы английского алфавита.
+	 */
+	ENGLISH(new EnglishScheme());
+
+	/**
+	 * Схема сопоставления.
+	 */
+	@NotNull
+	private final Schematic scheme;
+
+	/**
+	 * Конструктор для {@code Schemes}.
+	 *
+	 * @param scheme схема сопоставления.
+	 */
+	Schemes(@NotNull final Schematic scheme)
+	{
+		this.scheme = scheme;
+	}
+
+	/**
+	 * Возвращает схему сопоставления.
+	 *
+	 * @return схема сопоставления.
+	 */
+	@NotNull
+	public Schematic getScheme()
+	{
+		return this.scheme;
+	}
+}
